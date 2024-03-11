4fb95c9a52560c6d932615f58fce28cb18b4a215
romerenriquealvarezmartinez <romeralvareziv@gmail.com>
2024-03-11 11:53:15 +0000

    refactor(security): The SecurityConfig file has been moved to a new directory and expanded with additional security configurations.

--- src/main/java/me/romeralvarez/intellizenmarketmate/supermarket/domain/vo/SupermarketName.java
+++ src/main/java/me/romeralvarez/intellizenmarketmate/supermarket/domain/vo/SupermarketName.java
@@ -1,4 +1,13 @@
 package me.romeralvarez.intellizenmarketmate.supermarket.domain.vo;
 
 public class SupermarketName {
+    private final String name;
+
+    private SupermarketName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
