9d8e440ad54d9a9f6662d8c3e97b1a2ec99dbe24
romerenriquealvarezmartinez <romeralvareziv@gmail.com>
2024-03-14 12:32:34 +0000

    feat(IntelliZenMarketMateApplication class): Update class with annotation - Introduced an annotation to the IntelliZenMarketMateApplication class - This enhancement is aimed at increasing efficiency in managing, indexing, and retrieving domain event subscribers - It is also expected to aid in name-formatting for RabbitMQ

--- src/main/java/me/romeralvarez/intellizenmarketmate/IntelliZenMarketMateApplication.java
+++ src/main/java/me/romeralvarez/intellizenmarketmate/IntelliZenMarketMateApplication.java
@@ -1,9 +1,17 @@
 package me.romeralvarez.intellizenmarketmate;
 
+import me.romeralvarez.intellizenmarketmate.shared.domain.Service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(
+    includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Service.class),
+    value = {"me.romeralvarez"}
+
+)
 public class IntelliZenMarketMateApplication {
 
   public static void main(String[] args) {
