236ddb398f72fc839b22f9cfcaeb02fce0845576
romerenriquealvarezmartinez <romeralvareziv@gmail.com>
2024-03-14 12:23:49 +0000

    feat(RabbitMqIntegration): Add RabbitMqPublisher class for event handling - Introduced a new class, RabbitMqPublisher, to handle the publishing of domain events to RabbitMQ - The class contains methods for publishing via different exchange and routing options - It uses the DomainEventJsonSerializer for serializing the domain events before sending

---
+++ src/main/java/me/romeralvarez/intellizenmarketmate/shared/infraestructure/event/rabbitmq/RabbitMqPublisher.java
@@ -0,0 +1,36 @@
+package me.romeralvarez.intellizenmarketmate.shared.infraestructure.event.rabbitmq;
+
+import me.romeralvarez.intellizenmarketmate.shared.domain.Service;
+import me.romeralvarez.intellizenmarketmate.shared.domain.bus.event.DomainEvent;
+import me.romeralvarez.intellizenmarketmate.shared.infraestructure.DomainEventJsonSerializer;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePropertiesBuilder;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+@Service
+public final class RabbitMqPublisher {
+    private final RabbitTemplate rabbitTemplate;
+
+    public RabbitMqPublisher(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void publish(DomainEvent domainEvent, String exchangeName) throws AmqpException {
+        String serializedDomainEvent = DomainEventJsonSerializer.serialize(domainEvent);
+
+        Message message = new Message(
+            serializedDomainEvent.getBytes(),
+            MessagePropertiesBuilder.newInstance()
+                                    .setContentEncoding("utf-8")
+                                    .setContentType("application/json")
+                                    .build()
+        );
+
+        rabbitTemplate.send(exchangeName, domainEvent.eventName(), message);
+    }
+
+    public void publish(Message domainEvent, String exchangeName, String routingKey) throws AmqpException {
+        rabbitTemplate.send(exchangeName, routingKey, domainEvent);
+    }
+}
