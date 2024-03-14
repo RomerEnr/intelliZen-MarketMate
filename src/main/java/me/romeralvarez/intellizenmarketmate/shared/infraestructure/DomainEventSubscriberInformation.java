8185a723b0fdd9ba98ddc1dde7e7f5ad646fc208
romerenriquealvarezmartinez <romeralvareziv@gmail.com>
2024-03-14 12:29:22 +0000

    feat(shared infrastructure): Add DomainEventSubscribersInformation class - DomainEventSubscribersInformation, along with DomainEventSubscriberInformation, enhances scanning, managing, and indexing of domain event subscribers - These classes simplify data handling and retrieval of subscriber data - They also provide functionality to format names for RabbitMQ

---
+++ src/main/java/me/romeralvarez/intellizenmarketmate/shared/infraestructure/DomainEventSubscriberInformation.java
@@ -0,0 +1,34 @@
+package me.romeralvarez.intellizenmarketmate.shared.infraestructure;
+
+
+import me.romeralvarez.intellizenmarketmate.shared.domain.Utils;
+import me.romeralvarez.intellizenmarketmate.shared.domain.bus.event.DomainEvent;
+
+import java.util.List;
+
+
+public record DomainEventSubscriberInformation(Class<?> subscriberClass,
+                                               List<Class<? extends DomainEvent>> subscribedEvents) {
+
+    public String contextName() {
+        String[] nameParts = subscriberClass.getName().split("\\.");
+
+        return nameParts[2];
+    }
+
+    public String moduleName() {
+        String[] nameParts = subscriberClass.getName().split("\\.");
+
+        return nameParts[3];
+    }
+
+    public String className() {
+        String[] nameParts = subscriberClass.getName().split("\\.");
+
+        return nameParts[nameParts.length - 1];
+    }
+
+    public String formatRabbitMqQueueName() {
+        return String.format("romeralvarez.%s.%s.%s", contextName(), moduleName(), Utils.toSnake(className()));
+    }
+}
