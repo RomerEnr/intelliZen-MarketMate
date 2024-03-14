cc565447522ec1b66df4833ea78e3a2f15c862bf
romerenriquealvarezmartinez <romeralvareziv@gmail.com>
2024-03-14 12:37:14 +0000

    feat(Invoice): Add InvoiceCreated event - Updated the Invoice class to include the InvoiceCreated event - Allows recording of a new event whenever an Invoice is created, improving traceability of significant actions

--- src/main/java/me/romeralvarez/intellizenmarketmate/invoice/domain/Invoice.java
+++ src/main/java/me/romeralvarez/intellizenmarketmate/invoice/domain/Invoice.java
@@ -1,5 +1,6 @@
 package me.romeralvarez.intellizenmarketmate.invoice.domain;
 
+import me.romeralvarez.intellizenmarketmate.invoice.domain.events.InvoiceCreated;
 import me.romeralvarez.intellizenmarketmate.invoice.domain.vo.*;
 import me.romeralvarez.intellizenmarketmate.shared.domain.AggregateRoot;
 
@@ -20,5 +21,11 @@ public class Invoice extends AggregateRoot {
     this.imageUrl = imageUrl;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
+  }
+
+  public static Invoice create(InvoiceId id, Tax tax, ImageUrl imageUrl, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    Invoice invoice = new Invoice(id, tax, imageUrl, createdAt, updatedAt);
+    invoice.record(new InvoiceCreated(id.getValue().toString(), tax.value(), imageUrl.value(), createdAt, updatedAt));
+    return invoice;
   }
 }
