265a5c67e92edb9a229703eb2e6b9ac5f1cbcd30
romerenriquealvarezmartinez <romeralvareziv@gmail.com>
2024-03-14 12:31:28 +0000

    feat(shared infrastructure): Add annotation in DomainEventSubscriber class - Added a Java annotation to the DomainEventSubscriber class - The annotation will aid in the management and indexing of domain event subscribers - These changes enhance data handling and retrieval of subscriber data - The annotation will also assist with the formatting of names for RabbitMQ

--- src/main/java/me/romeralvarez/intellizenmarketmate/shared/domain/bus/event/DomainEventSubscriber.java
+++ src/main/java/me/romeralvarez/intellizenmarketmate/shared/domain/bus/event/DomainEventSubscriber.java
@@ -1,4 +1,11 @@
 package me.romeralvarez.intellizenmarketmate.shared.domain.bus.event;
 
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
 public @interface DomainEventSubscriber {
+  Class<? extends DomainEvent>[] value();
 }
